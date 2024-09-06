9244379cbb397077f2fa824c5d439f363754f949
Borinith <borinith@gmail.com>
2024-09-06 23:00:11 +0300

    Add simple WebSocket client

---
+++ SuperPlayClient/SuperPlayClient/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SuperPlayClient
+{
+    public static class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            using (var ws = new ClientWebSocket())
+            {
+                // todo log
+                await ws.ConnectAsync(new Uri("wss://localhost:7182/ws"), CancellationToken.None);
+                // todo log
+
+                var buffer = new byte[1024 * 4];
+
+                while (true)
+                {
+                    var result = await ws.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+
+                    if (result.MessageType == WebSocketMessageType.Close)
+                    {
+                        // todo log
+                        break;
+                    }
+
+                    var message = Encoding.UTF8.GetString(buffer, 0, result.Count);
+
+                    Console.Write(message + "\r");
+                    // todo log
+                }
+            }
+        }
+    }
+}
